570e538b5fafebd61f7192224816c780abec8bb3
dianakakoma <dikakoma@gmail.com>
2020-09-06 17:39:42 -0500

    created the 5 RESTful API routes.

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    it "creates a new report" do
+      report = Report.create(name: "Test",description: "there is an insect in my computer, suggested_fix: "Call Terminix”, url:"test url", status: "test status", screenshot: "https://cdn.pixabay.com/photo/2015/05/11/13/28/software-762486_960_720.jpg")
+      expect(user).to be_valid
+    end
+  end
 end
--- spec/requests/api/reports_request_spec.rb
+++ spec/requests/api/reports_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Reports", type: :request do
-
+  describe 'create' do
+    it "successfully creates a new report" do
+      report = Report.create(name: "Test Name",description: "there is an insect in my computer, suggested_fix: "Call Terminix”, url:"test url", status: "test status", screenshot: "https://cdn.pixabay.com/photo/2015/05/11/13/28/software-762486_960_720.jpg")
+      expect(Report.name).to eq("Test Name")
+    end
+  end   
 end
